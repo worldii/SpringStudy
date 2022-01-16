0e491a9bdaa14ae9a603b75ab32b1346b0afec88
worldi <jongha2788@u.sogang.ac.kr>
2022-01-17 04:05:42 +0900

    Spring bin 으로 전환

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -8,19 +8,25 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    // 스프링 컨테이너에 등록이 됨.
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl( memberRepository(), discountPolicy());
     }
+    @Bean
     public DiscountPolicy discountPolicy() {
       //  return new FixDiscountPolicy();
         return new RateDiscountPolicy();
